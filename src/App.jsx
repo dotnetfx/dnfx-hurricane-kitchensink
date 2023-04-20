bd04aaa693065c664da2a554d6d55334c5375082
Derek Rowley <derekrowley@dotnetfx.com>
2023-04-20 16:29:11 -0600

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Card } from 'dnfx-hurricane-blocks'
+
+export default function App() {
+  return (
+    <div className="p-4">
+      <Card title="Card Block" description="Lorum ispum dolor sit amit..." />
+    </div>
+  )
+}
